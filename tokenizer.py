c0c4524a4fdac0e381f501a829edeb5c1c6fede8
FreeCafeWi-Fi <dhiaan@outlook.com>
2026-02-11 14:41:24 +1100

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,16 @@
 def tokenize(text):
-  word = text.split()
+next_id = 1
 
-  word_to_id = {}
-  next_id = 1
+def tokenize(text):
+  global word_to_id, next_id
+
+  words = text.split()
+  
   for w in word:
       if w not in word_to_id:
         word_to_id[w] = next_id
         next_id+= 1
+        
   ids = [word_to_id[w] for w in word]
   return ids, word_to_id
   
