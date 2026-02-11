d659eb1c7492209ab7208b03e1cf84017aac03ed
FreeCafeWi-Fi <dhiaan@outlook.com>
2026-02-11 22:48:51 +1100

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,1 +1,7 @@
+def generate(model, tokenizer, prompt, max_new_tokens=30):  
+    ids, _ = tokenizer.tokenize(prompt)
 
+    for _ in range(max_new_tokens):
+        context = ids{-3:}
+
+          
